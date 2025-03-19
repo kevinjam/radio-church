e36a086d14caba2f28751bf566b77a61be77b174
kevinjam <kevin.janvier5@gmail.com>
2025-03-19 21:02:59 +0300

    Resolved merge conflicts

---
+++ next.config.ts
@@ -0,0 +1,60 @@
+// next.config.ts
+import withPWA from "next-pwa";
+import type { NextConfig } from "next";
+
+// Define the base Next.js configuration
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://chat.radiojar.com;",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://www.google.com/;",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.castbox.fm",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/api/image/proxy/**",
+      },
+      {
+        protocol: "https",
+        hostname: "radiojar.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+// Wrap the config with PWA settings
+export default withPWA({
+  dest: "public", // Output directory for service worker files
+  register: true, // Automatically register the service worker
+  skipWaiting: true, // Skip waiting for the service worker to take control
+})(nextConfig);
