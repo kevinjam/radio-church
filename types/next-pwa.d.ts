2307b2e84c31ecf07951e71bad068d2efa5fa98d
kevinjam <kevin.janvier5@gmail.com>
2025-03-19 21:13:25 +0300

    adding readme

---
+++ types/next-pwa.d.ts
@@ -0,0 +1,16 @@
+// types/next-pwa.d.ts
+declare module "next-pwa" {
+    import { NextConfig } from "next";
+  
+    interface PWAConfig {
+      dest: string;
+      register?: boolean;
+      skipWaiting?: boolean;
+      disable?: boolean;
+      [key: string]: unknown;
+    }
+  
+    function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig;
+  
+    export default withPWA;
+  }
